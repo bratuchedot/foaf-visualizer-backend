ce5fc60856965a2fdf3ed71ffbfd3fae731dbd15
Emilijan Koteski <emilijankoteski@pm.me>
2023-07-29 11:49:42 +0200

    first commit

---
+++ src/main/java/mk/ukim/finki/foafvisualizerbackend/FoafVisualizerApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.foafvisualizerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoafVisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoafVisualizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mk/ukim/finki/foafvisualizerbackend/FoafVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.foafvisualizerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoafVisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
