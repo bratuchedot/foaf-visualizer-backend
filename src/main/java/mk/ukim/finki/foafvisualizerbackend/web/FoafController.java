2671f6f542c295efd7e6dcc6d65582d6cb8ba7c5
Emilijan Koteski <emilijankoteski@pm.me>
2023-08-13 19:44:17 +0200

    changes in foaf controller

--- src/main/java/mk/ukim/finki/foafvisualizerbackend/web/FoafController.java
+++ src/main/java/mk/ukim/finki/foafvisualizerbackend/web/FoafController.java
@@ -3,17 +3,17 @@ package mk.ukim.finki.foafvisualizerbackend.web;
 import lombok.AllArgsConstructor;
 import mk.ukim.finki.foafvisualizerbackend.model.FoafResponse;
 import mk.ukim.finki.foafvisualizerbackend.service.FoafService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @AllArgsConstructor
 @RequestMapping("/foaf")
 public class FoafController {
 
     private final FoafService service;
 
+    @GetMapping
     @RequestMapping("/analyze")
     public FoafResponse analyze(@RequestParam String foafUrl) {
         return service.analyze(foafUrl);
